62d71a5b8a71b7c3411f1f505c2aa3541171b549
Charlie Billen <charlesbillen@gmail.com>
2022-06-14 20:13:15 +0100

    Brings back some test coverage lost when making ShuntingYard.Core classes internal

--- ShuntingYard.Core.Tests/ShuntingYardCalculatorTests.cs
+++ ShuntingYard.Core.Tests/ShuntingYardCalculatorTests.cs
@@ -4,10 +4,12 @@ public class ShuntingYardCalculatorTests
 {
     [Theory]
     [InlineData("1+2", 3)]
+    [InlineData("1-2", -1)]
     [InlineData("1+2*3", 7)]
     [InlineData("1*2/3", 0.667)]
     [InlineData("(1+2)*3", 9)]
     [InlineData("(1+2)*(3+4)", 21)]
+    [InlineData("100+200", 300)]
     public void Solve_GivenPostfixCalculation_ReturnsTheResult(string calculation, decimal expected)
     {
         var calculator = new ShuntingYardShuntingYardCalculator();
--- ShuntingYard.Core/Internal/Tokens/Token.cs
+++ ShuntingYard.Core/Internal/Tokens/Token.cs
@@ -1,37 +1,17 @@
+using System.Diagnostics.CodeAnalysis;
+
 namespace ShuntingYard.Core.Internal.Tokens;
 
-internal abstract class Token : IEquatable<Token>
+internal abstract class Token
 {
-    public string Value { get; protected init; } = string.Empty;
-    public int Precedence { get; protected init; }
+    protected string Value { get; init; } = string.Empty;
+    protected int Precedence { get; init; }
 
     public static bool operator <=(Token left, Token right)
         => left.Precedence <= right.Precedence;
 
+    // This must be defined to define <= but is not used in this code
+    [ExcludeFromCodeCoverage]
     public static bool operator >=(Token left, Token right)
         => left.Precedence >= right.Precedence;
-
-    #region IEquatable members implemented by Rider
-
-    public bool Equals(Token? other)
-    {
-        if (ReferenceEquals(null, other)) return false;
-        if (ReferenceEquals(this, other)) return true;
-        return Value == other.Value && Precedence == other.Precedence;
-    }
-
-    public override bool Equals(object? obj)
-    {
-        if (ReferenceEquals(null, obj)) return false;
-        if (ReferenceEquals(this, obj)) return true;
-        if (obj.GetType() != this.GetType()) return false;
-        return Equals((Token)obj);
-    }
-
-    public override int GetHashCode()
-    {
-        return HashCode.Combine(Value, Precedence);
-    }
-    
-    #endregion
 }
