d2a4c5e68fde2510e660c968999338ee4eefdb9a
Charlie Billen <charlesbillen@gmail.com>
2022-06-17 19:55:20 +0100

    Tidies usings

--- ShuntingYardCalculator.Benchmarks/ShuntingYardCalculatorBenchmark.cs
+++ ShuntingYardCalculator.Benchmarks/ShuntingYardCalculatorBenchmark.cs
@@ -1,11 +1,12 @@
 using BenchmarkDotNet.Attributes;
+using Calculator = ShuntingYard.Core.ShuntingYardCalculator;
 
 namespace ShuntingYardCalculator.Benchmarks;
 
 [MemoryDiagnoser]
 public class ShuntingYardCalculatorBenchmark
 {
-    private readonly ShuntingYard.Core.ShuntingYardCalculator _calculator = new ShuntingYard.Core.ShuntingYardCalculator();
+    private readonly Calculator _calculator = new();
     private readonly string _calculation = "((100+200)*(3000-4000))/2";
 
     [Benchmark]
